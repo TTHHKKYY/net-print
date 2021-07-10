16a1a8a7213bb3e4436d07edf8c554d73f696ddc
TTHHKKYY <THKY2@protonmail.com>
2021-07-10 01:22:15 -0600

    Initial commit

---
+++ client/client.lua
@@ -0,0 +1,26 @@
+local modem = peripheral.find("modem")
+
+local arguments = {...}
+local file = arguments[1]
+
+assert(modem,"No modem.")
+assert(modem.isWireless(),"Modem is not rednet capable.")
+assert(file,"No file.")
+
+local port = 65088
+local reply = 65087
+
+local input = io.open(file,"r")
+
+local content = {}
+local out = {}
+
+for line in input:lines() do
+	table.insert(content,line)
+end
+
+out[1] = file
+out[2] = table.concat(content)
+
+modem.transmit(port,reply,out)
+print("Sent.")
---
+++ server/main.lua
@@ -0,0 +1,53 @@
+local modem = peripheral.find("modem")
+local printer = peripheral.find("printer")
+
+assert(printer,"No printer.")
+assert(modem,"No modem.")
+assert(modem.isWireless(),"Modem is not rednet capable.")
+
+local incoming = 65088
+local reply = 65087
+
+modem.open(incoming)
+modem.open(reply)
+
+while true do
+	local event,_,port,_,data = os.pullEventRaw()
+	
+	if event == "terminate" then
+		modem.close(incoming)
+		modem.close(reply)
+		print("Stopped Server")
+		
+		return
+	end
+	if event == "modem_message" and port == incoming then
+		local name = data[1]
+		local content = data[2]
+		
+		printer.newPage()
+		printer.setPageTitle(name)
+		print("Printing...")
+		
+		for i=1,#content do
+			
+			if i % printer.getPageSize() == 0 then
+				if printer.getPaperLevel() == 0 then
+					print("Ran out of paper.")
+					break
+				end
+				
+				printer.newPage()
+				printer.setPageTitle(name)
+			end
+
+			if printer.getInkLevel() == 0 then
+				print("Ran out of dye.")
+				break
+			end
+		end
+		
+		printer.endPage()
+		print("Done.")
+	end
+end
